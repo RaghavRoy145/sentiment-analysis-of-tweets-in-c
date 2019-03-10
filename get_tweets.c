f0f1bb69655dfc361b3af437469d0969d9800094
Samyak2 <samyak201@gmail.com>
2019-03-10 20:38:26 +0530

    Added comments and cleaned up code

--- get_tweets.c
+++ get_tweets.c
@@ -6,6 +6,7 @@
 
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
 {
+    // Write to output file
     FILE *fptr;
     fptr = fopen("output.json", "a");
     fprintf(fptr,"%s", buffer);
@@ -32,6 +33,7 @@ int tweets_get()
     char *signature = malloc(buf_size);
     if (signature == NULL) return 1;
 
+    // Get Twitter API keys from file 'keys'
     FILE *keys = fopen("keys", "r"); int len = 64;
     char *consumer_key = malloc(64);
     char *consumer_secret = malloc(64);
@@ -43,32 +45,34 @@ int tweets_get()
     fgets(access_token_secret, len, keys); access_token_secret[strcspn(access_token_secret, "\n")] = 0;
     fclose(keys);
 
+    // Calculate key from API keys
     char *hmac_key = malloc(buf_size);
-    if (hmac_key == NULL) return 1;
+    if(hmac_key == NULL) return 1;
     strcat(hmac_key, consumer_secret);
     strcat(hmac_key, "&");
     strcat(hmac_key, access_token_secret);
 
+    // Calculate signature 
     strcpy(signature, compute_signature(timestamp, nonce, consumer_key, access_token, hmac_key));
 
+    // Create request to be sent
     char *post = malloc(buf_size);
-    if (post == NULL) {
-        return 1;
-    }
+    if(post == NULL) return 1;
     strcpy(post, create_post(timestamp, nonce, signature, consumer_key, access_token));
 
+    // Initialize curl
     CURL *curl;
-    CURLcode res;
-    
     curl = curl_easy_init();
     if(curl) {
+        // Set request paramters like url, header, timeout
         struct curl_slist *chunk = NULL;
         chunk = curl_slist_append(chunk, post);
         curl_easy_setopt(curl, CURLOPT_URL, "https://stream.twitter.com/1.1/statuses/sample.json");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
         curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        res = curl_easy_perform(curl);
+        // Send curl request
+        curl_easy_perform(curl);
         curl_easy_cleanup(curl);
     }
     return 0;
--- parse_output.c
+++ parse_output.c
@@ -3,6 +3,88 @@
 #include <string.h>
 #include <locale.h>
 #include "mylibs.h"
+void extract_required(char *tweet_text, char *tweet);
+void deleteChars(char *s, char c);
+int deleteWord(char *s, char *word);
+char **processTweet(char *tweet);
+
+int main()
+{
+    // Open output.json file to read
+    FILE *fptr;
+    size_t len = 10000;
+    fptr = fopen("output.json", "r");
+    char *tmp = malloc(sizeof(char) * 10000);
+    // Get maximum number of tweets to extract from user
+    int n;
+    printf("Enter maximum number of tweets to extract: ");
+    scanf("%d", &n);
+    char tweet_texts[n][100000], tweet_usernames[n][1000];
+    char tweets[n][10000], usernames[n][100];
+    char *text_start = NULL, *text_end = NULL;
+    char *username_start = NULL, *username_end = NULL;
+    int i = 0;
+
+    // Read output file line by line
+    while (i < n && (getline(&tmp, &len, fptr)) != -1)
+    {
+        // Extract only tweet and username
+        text_start = strstr(tmp, "\"text\"");
+        text_end = strstr(tmp, ",\"source\"");
+        username_start = strstr(tmp, "\"screen_name\":");
+        username_end = strstr(tmp, ",\"location\"");
+
+        if (strncmp("{\"created_at\":", tmp, 14) == 0 && strnstr(text_start, "\\u", text_end - text_start) == NULL && i < n)
+        {
+            strncpy(tweet_texts[i], text_start, text_end - text_start);
+            //printf("%s\n", tweet_texts[i]);
+            strncpy(tweet_usernames[i], username_start, username_end - username_start);
+            i++;
+        }
+    }
+    fclose(fptr);
+    if(tmp) free(tmp);
+
+    // print if no tweet is extracted
+    if (strcmp(tweet_texts[0], "") == 0)
+    {
+        printf("Could not find a tweet\n");
+        return 0;
+    }
+
+    //printf("%s\n", tweet_text);
+    //printf("%s\n", tweet_username);
+    char **words, *word;
+    int k = 0;
+    int count = 0;
+    // Extract only tweet and process it
+    for(int j=0; j < i; ++j)
+    {
+        extract_required(tweet_texts[j], tweets[j]);
+        // printf("Tweet: %s\n", tweets[j]);
+        words = processTweet(tweets[j]);
+        if(words == NULL || words[0] == NULL) continue;
+        else if(words != NULL)
+        {
+            printf("Tweet: ");
+            k = 0;
+            while((word = words[k]) != NULL)
+            {
+                // if(strcmp(word, "") == 0) continue;
+                printf("%s ", word);
+                ++k;
+            }
+            printf("\n");
+            ++count;
+            free(words);
+        }
+        // printf("Tweet: %s\n", tweets[j]);
+        extract_required(tweet_usernames[j], usernames[j]);
+        printf("Username: @%s\n", usernames[j]);
+    }
+    printf("%d tweets found.\n", count);
+    return 0;
+}
 
 
 void extract_required(char *tweet_text, char *tweet)
@@ -13,7 +95,7 @@ void extract_required(char *tweet_text, char *tweet)
     strcpy(tweet, req);
 }
 
-// from test.c
+
 void deleteChars(char *s, char c)
 {
     int writer = 0, reader = 0;
@@ -56,7 +138,7 @@ char **processTweet(char *tweet)
     if(strlen(tweet) < 5) return NULL;
     char *result = (char *) malloc(sizeof(char) * strlen(tweet)), *tmp;
     char stopwords[][32] = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "you're", "you've", "you'll", "you'd", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it"};
-    char punctuation[] = ".!,'@?()/~_-\\:[]{}";
+    char punctuation[] = ".!,'?()/~_-\\:[]{}";
     strcpy(result, tweet);
     
     // to lowercase
@@ -68,6 +150,7 @@ char **processTweet(char *tweet)
     // if(tmp != NULL) *tmp = '\0';
     deleteWord(result, "http");
     deleteWord(result, "rt");
+    deleteWord(result, "@");
 
     //remove punctuatuon symbols
     for(int i = 0; i < strlen(punctuation); ++i)
@@ -124,80 +207,3 @@ char **processTweet(char *tweet)
     // return words2;
 }
 // end 
-
-int main()
-{
-    FILE *fptr;
-    size_t len = 10000;
-    fptr = fopen("output.json", "r");
-    char *tmp = malloc(sizeof(char) * 10000);
-    int n = 10;
-    printf("Enter maximum number of tweets to extract: ");
-    // n = 5;
-    scanf("%d", &n);
-    char tweet_texts[n][100000], tweet_usernames[n][1000];
-    char tweets[n][10000], usernames[n][100];
-    char *text_start = NULL, *text_end = NULL;
-    char *username_start = NULL, *username_end = NULL;
-    int cont = 0, i = 0;
-    while (i < n && (getline(&tmp, &len, fptr)) != -1)
-    {
-        //printf("%s\n", tmp);
-        text_start = strstr(tmp, "\"text\"");
-        text_end = strstr(tmp, ",\"source\""); //",\"source\"");
-        username_start = strstr(tmp, "\"screen_name\":");
-        username_end = strstr(tmp, ",\"location\"");
-
-        if (strncmp("{\"created_at\":", tmp, 14) == 0 && strnstr(text_start, "\\u", text_end - text_start) == NULL && i < n)
-        {
-            //strcpy(tweet_texts[i], "");
-            //printf("%p\n", strnstr(text_start, "\\u", text_end-text_start));
-            strncpy(tweet_texts[i], text_start, text_end - text_start);
-            //printf("%s\n", tweet_texts[i]);
-            strncpy(tweet_usernames[i], username_start, username_end - username_start);
-            i++;
-            //printf("%s\nSelect another tweet?: ", tweet_text);
-            //scanf("%d", &cont);
-            //if(cont == 0) break;
-        }
-    }
-    if (strcmp(tweet_texts[0], "") == 0)
-    {
-        printf("Could not find a tweet\n");
-        return 0;
-    }
-    //printf("%s\n", tweet_text);
-    //printf("%s\n", tweet_username);
-    fclose(fptr);
-    if (tmp)
-        free(tmp);
-    char **words, *word;
-    int k = 0;
-    int count = 0;
-    for(int j=0; j < i; ++j)
-    {
-        extract_required(tweet_texts[j], tweets[j]);
-        // printf("Tweet: %s\n", tweets[j]);
-        words = processTweet(tweets[j]);
-        if(words == NULL || words[0] == NULL) continue;
-        else if(words != NULL)
-        {
-            printf("Tweet: ");
-            k = 0;
-            while((word = words[k]) != NULL)
-            {
-                // if(strcmp(word, "") == 0) continue;
-                printf("%s ", word);
-                ++k;
-            }
-            printf("\n");
-            ++count;
-            free(words);
-        }
-        // printf("Tweet: %s\n", tweets[j]);
-        extract_required(tweet_usernames[j], usernames[j]);
-        printf("Username: @%s\n", usernames[j]);
-    }
-    printf("%d tweets found.\n", count);
-    return 0;
-}
